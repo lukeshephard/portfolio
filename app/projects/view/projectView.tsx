9987d750b568f767368d64fa81a9d8865c133160
shephardluke <shephardluke@hotmail.com>
2025-02-09 22:13:19 +0000

    Add back link on project information page
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/projects/view/projectView.tsx
+++ app/projects/view/projectView.tsx
@@ -2,8 +2,10 @@
 import { useSearchParams } from "next/navigation"
 import { Project } from "../project";
 import ProjectInformation from "./projectInformation";
+import { useRouter } from "next/navigation";
 
 export default function ProjectView() {
+    const router = useRouter()
 
     const searchParams = useSearchParams();
     const searchName = searchParams.get("name") as string;
@@ -14,8 +16,11 @@ export default function ProjectView() {
 
     return (
         <>
-            <h1>{project ? project.getTitle() : "Project not found."}</h1>
-            {projectInformation}
+            <button className="linkStyle" onClick={() => {router.back()}}>Back</button>
+            <div className="pt-5">
+                <h1>{project ? project.getTitle() : "Project not found."}</h1>
+                {projectInformation}
+            </div>
         </>
     )
 }
