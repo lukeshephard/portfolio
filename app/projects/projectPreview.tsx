b5b91696124bd0028f2585c8a78586df2f109123
shephardluke <shephardluke@hotmail.com>
2025-02-18 20:43:14 +0000

    Add better mouse response on projects page
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/projects/projectPreview.tsx
+++ app/projects/projectPreview.tsx
@@ -3,11 +3,12 @@ import ProjectMainImage from "./projectMainImage";
 
 export default function ProjectPreview({project}: {project: Project}) { // Show project as a preview
     return (
-        <div className="p-5 hover:border-neutral-400 bg-darkest-blue">
+        <div className="p-5 bg-darkest-blue hover:bg-darkest-blue-2x active:bg-darkest-blue-3x hover:text-neutral-400 active:text-neutral-600">
             <a href={project.getInformationLink().getLink()}>
-                <p className="pb-3 text-2xl">{project.getTitle()}</p>
+                <p className="pb-5 text-2xl">{project.getTitle()}</p>
                 <p>{project.getSummary()}</p>
-                <ProjectMainImage project={project} className="mt-3 max-h-96 w-auto border m-auto"/>
+                <ProjectMainImage project={project} className="mt-5 max-h-96 w-auto border m-auto"/>
+                <p className="mt-5">Created: {project.getCreated().format("DD/MM/YYYY")}</p>
             </a>
         </div>
     )
