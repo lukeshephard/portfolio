deb33d2d3db50a3572752844f5b550efc2e31df4
shephardluke <shephardluke@hotmail.com>
2025-03-03 09:40:27 +0000

    Revert code
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/utils/formatDate.tsx
+++ app/utils/formatDate.tsx
@@ -2,47 +2,17 @@
 
 import dayjs, { Dayjs } from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 dayjs.extend(relativeTime)
 
-setInterval(updateDates, 60000)
-const dates: DateUpdater[] = [];
-
-
-class DateUpdater { // Takes dates and their set methods for updating
-    private date: Dayjs
-    private setMethod: Dispatch<SetStateAction<string>>
-
-    constructor(date: Dayjs, setMethod: Dispatch<SetStateAction<string>>) {
-        this.date = date;
-        this.setMethod = setMethod;
-    }
-
-    getDate() {
-        return this.date;
-    }
-
-    getSetMethod() {
-        return this.setMethod;
-    }
-}
-
-function updateDates() { // Updates global date array when the interval goes off
-    console.log("updating " + dates.length + " dates...")
-    for (const date of dates) {
-        date.getSetMethod()(date.getDate().fromNow());
-    }
-
-}
 
 export function FormatDate({date} : {date: Dayjs}) { // Returns the given date as a string in a special format
     const [isClient, setIsClient] = useState(false)
-    const [dateString, setDateString] = useState(date.fromNow())
+    const [dateString] = useState(date.fromNow())
 
     useEffect(() => {
         setIsClient(true)
-        dates.push(new DateUpdater(date, setDateString))
     }, [])
 
     if (!date.isValid()) {
