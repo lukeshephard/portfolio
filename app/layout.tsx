53ca4e29d83afc4d7e4f36fe8d4e3b32e7e3ae38
shephardluke <shephardluke@hotmail.com>
2025-08-10 17:49:44 +0100

    Update tailwind to v4 and all packages to latest
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ContentWrapper from "./content-wrapper/contentWrapper";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,13 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ContentWrapper>
-          {children}
-        </ContentWrapper>
+        <ThemeProvider>
+          <ContentWrapper>
+            {children}
+          </ContentWrapper>
+        </ThemeProvider>
       </body>
     </html>
   );
