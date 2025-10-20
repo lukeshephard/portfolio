8507dfdda4b187f136529cc1b70e0e58a77168a1
shephardluke <shephardluke@hotmail.com>
2025-10-20 21:10:06 +0100

    Add character typing
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/typewriter/codeTypewriter.tsx
+++ app/typewriter/codeTypewriter.tsx
@@ -9,62 +9,106 @@ hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('xml', xml);
 
 export default function CodeTypewriter() {
+    const [typewriterCode, setTypewriterCode] = useState<string | null>(null);
     const codeElement = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        setTypewriterCode(codeList[0]);
+    }, [])
+
 
     // Cycle through typing code
     useEffect(() => {
+        if (!codeElement.current || typewriterCode === null) return;
 
         const hiddenCodeElement = document.createElement("code")
-        hiddenCodeElement.innerHTML = hljs.highlight(codeList[0], {language: "tsx"}).value
-        // recursivelyType(hiddenCodeElement.childNodes[0])
-        console.log(hiddenCodeElement.childNodes)
-        console.log(hiddenCodeElement.children[7].childNodes[1].textContent)
+        hiddenCodeElement.innerHTML = hljs.highlight(typewriterCode, {language: "tsx"}).value.replaceAll("    ", "\t")
 
-        const allNodes: Node[] = [];
-        console.log(allNodes)
+
+        const CURSOR = document.createTextNode("|");
+        const CHAR_DELAY = 50;
+
+        codeElement.current.innerHTML = "";
 
         const pointer = {
             displayedParentNode: codeElement.current as Node,
             hiddenNode: hiddenCodeElement.childNodes[0],
-            finished: false
+            currentChar: 0,
         }
 
-        const interval = setInterval(() => type(pointer), 75)
-        function type(pointer: {displayedParentNode: Node, hiddenNode: Node, finished: boolean}) {
-            if (pointer.finished || !codeElement.current) return;
+        setTimeout(() => type(pointer), CHAR_DELAY)
+
+        function onComplete() {
+            console.log("COMPLETE")
+        }
+    
+        // Type the next character / tag, runs on timeouts until all of hiddenCodeElement is typed into codeElement
+        // Starts at first child of hiddenCodeElement
+        // Decides next node (hiddenNode) to type by doing a depth first search: 1. Itself, 2. First Child, 3.Next sibling, 3. Parent's next sibling
+        function type(pointer: {displayedParentNode: Node, hiddenNode: Node, currentChar: number}) {
+            if (!codeElement.current) return;
+
+            if (pointer.displayedParentNode.lastChild === CURSOR) {
+                pointer.displayedParentNode.removeChild(CURSOR)
+            }
             
-
-
             const hiddenParentNode = pointer.hiddenNode.parentNode as Node;
-            const hiddenNodeIndex = pointer.displayedParentNode.childNodes.length;
-
-            console.log(pointer.hiddenNode, hiddenNodeIndex);
-
+            let hiddenNodeIndex = pointer.displayedParentNode.childNodes.length;
             const displayedParentParentNode = pointer.displayedParentNode.parentNode as Node;
 
-            pointer.displayedParentNode.appendChild(pointer.hiddenNode.cloneNode(false));
+            let delay = CHAR_DELAY;
 
+            // If text node, type char by char, otherwise paste the whole tag
+            if (pointer.hiddenNode.nodeType === Node.TEXT_NODE) {
+                const text = pointer.hiddenNode.textContent as string;
+                hiddenNodeIndex = pointer.displayedParentNode.childNodes.length - 1;
 
-            if (pointer.hiddenNode.hasChildNodes()) { // 1. Next child
-                console.log(1)
+                if (pointer.currentChar !== text.length) {
+                    if (pointer.currentChar === 0) {
+                         pointer.displayedParentNode.appendChild(document.createTextNode(text[pointer.currentChar]));
+                    } else {
+                        pointer.displayedParentNode.childNodes[hiddenNodeIndex].textContent += text[pointer.currentChar];
+                    }
+
+                    pointer.currentChar += 1;
+                    if (text[pointer.currentChar] === "\t") {
+                        delay = 0;
+                    }
+                    
+                    onEnd();
+                    return;
+                }
+
+                pointer.currentChar = 0
+            } else {
+                pointer.displayedParentNode.appendChild(pointer.hiddenNode.cloneNode(false));
+                delay = 0;
+            }
+
+            // The cycle goes -> 
+            if (pointer.hiddenNode.hasChildNodes()) { 
                 pointer.hiddenNode = pointer.hiddenNode.childNodes[0];
                 pointer.displayedParentNode = pointer.displayedParentNode.childNodes[hiddenNodeIndex];
-                //console.log(pointer.hiddenNode, pointer.displayedParentNode, hiddenNodeIndex)
-            } else if (hiddenParentNode.childNodes.length > hiddenNodeIndex + 1) { // 2. Next sibling
-                console.log(2)
+            } else if (hiddenParentNode.childNodes.length > hiddenNodeIndex + 1) {
                 pointer.hiddenNode = hiddenParentNode.childNodes[hiddenNodeIndex + 1];
-            } else if (pointer.displayedParentNode !== codeElement.current && (hiddenParentNode.parentNode as Node).childNodes.length > displayedParentParentNode.childNodes.length) { // 3. Next parent's sibling
-                console.log(3,  displayedParentParentNode.childNodes.length)
+            } else if (pointer.displayedParentNode !== codeElement.current && (hiddenParentNode.parentNode as Node).childNodes.length > displayedParentParentNode.childNodes.length) {
                 pointer.displayedParentNode = displayedParentParentNode;
                 pointer.hiddenNode = (hiddenParentNode.parentNode as Node).childNodes[displayedParentParentNode.childNodes.length]
             } else {
-                clearInterval(interval)
+                onComplete();
+                return;
             }
-            
-            // console.log(codeElement.current?.innerHTML)
-        }
-    }, [])
 
+            onEnd();
+        
+            function onEnd() {
+                if (delay !== 0) {
+                    pointer.displayedParentNode.appendChild(CURSOR);
+                }
+                setTimeout(() => type(pointer), delay);
+            }
+        }
+    }, [typewriterCode])
 
     return (
         <pre className="h-full overflow-hidden select-none pointer-events-none text-[2.5vh] transition-opacity ease-in-out duration-[3s]">
