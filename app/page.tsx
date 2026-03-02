89923a5a39737577fab82d9b960bcd4b665b57f6
shephardluke <shephardluke@hotmail.com>
2026-03-02 19:56:34 +0000

    Fix text
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <div className="backdrop-blur-md backdrop-opacity-100 px-9 rounded-4xl md:p-9 md:border-1">
           {/* <pre className="text-sm text-center whitespace-pre text-logo py-12">{getFullName()}</pre> */}
           <h1 className={`text-4xl md:text-6xl md:text-7xl 2xl:text-8xl text-logo md:py-12 ${shadowsIntoLight.className}`}>Luke Shephard</h1>
-          <h2 className={`text-xl md:text-xl md:text-2xl md:text-3xl 2xl:text-5xl py-6`}>Final Year Computer Science Student.</h2> 
+          <h2 className={`text-xl md:text-xl md:text-2xl md:text-3xl 2xl:text-5xl py-6`}>Final-Year Computer Science Student.</h2> 
           <div className="flex justify-center">
             <h3 className="md:text-md md:text-xl 2xl:text-3xl">Open for Graduate Roles in Software Engineering.</h3>
           </div>
@@ -24,7 +24,7 @@ export default function Home() {
       </div>
       <div className="pt-10 md:p-0 text-xs md:text-sm 2xl:text-lg mb-3">
         <hr className="mb-3 md:hidden" />
-        <p>Version 0.5.3 - 2025-10-18</p>
+        <p>Version 0.5.3 - 2026-03-02</p>
         <div className="flex justify-center gap-3">
           <a className="flex justify-center" target="_blank" rel="noopener noreferrer" href="https://github.com/lukeshephard/portfolio"><Code className="my-auto mr-1"/>Repository</a>
           <p> | </p>
